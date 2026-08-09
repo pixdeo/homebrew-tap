bce0413298a794d078f7235178a4e9a47dc2647c
Martin Romañuk <mromanuk@me.com>
2026-08-08 22:20:26 -0300

    editxr 1.6.3

--- Formula/editxr.rb
+++ Formula/editxr.rb
@@ -1,9 +1,9 @@
 class Editxr < Formula
   desc "Minimalist Markdown editor for the terminal"
   homepage "https://editxr.org"
-  url "https://github.com/pixdeo/editxr/releases/download/v1.3.0/editxr-v1.3.0-macos-universal.zip"
-  sha256 "71127f697b492375ff49dc94d16e736ac005683f3a2a879474a3c046a54875b5"
-  version "1.3.0"
+  url "https://github.com/pixdeo/editxr/releases/download/v1.6.3/editxr-v1.6.3-macos-universal.zip"
+  sha256 "c1dc917265e853b342d2baef5c7720bc39e1a6b42a996e986365805b8d64e003"
+  version "1.6.3"
   license "MIT"
 
   # Prebuilt Developer ID-signed, notarised universal (arm64 + x86_64) binary.
